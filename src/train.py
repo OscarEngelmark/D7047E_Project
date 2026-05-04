0056bcfebfb1a7b1a232ec647860b354ab2c8cdd
Oscar <oscar.luen@proton.me>
2026-05-04 17:20:17 +0200

    Set to save ckpt every 5 epochs (10)

--- src/train.py
+++ src/train.py
@@ -322,7 +322,7 @@ def resolve_train_kwargs(
         "patience":     args.patience,
         "freeze":       args.freeze if args.freeze > 0 else None,
         "close_mosaic": 0,
-        "save_period":  10,
+        "save_period":  5,
         "compile":      torch.cuda.is_available(),
         "device":       DEVICE,
         "seed":         g.SEED,
